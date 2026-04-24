0be3adce4d16ba891775a0cd4fd5ad65bd23859a
s3rdia <s3rdia@web.de>
2026-04-24 23:14:21 +0200

    no_color auto initializing

--- R/globals.R
+++ R/globals.R
@@ -37,4 +37,4 @@
 .printify_messages[["last_session"]]   <- NULL
 .printify_messages[["last_execution"]] <- NULL
 .printify_messages[["no_print"]]       <- FALSE
-.printify_messages[["no_color"]]       <- FALSE
+.printify_messages[["no_color"]]       <- nzchar(Sys.getenv("NO_COLOR"))
